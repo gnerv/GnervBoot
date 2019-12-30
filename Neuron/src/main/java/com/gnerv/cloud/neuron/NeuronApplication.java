1040fc6be0100dc433687c25b4983b2282e821c1
lgnerv <ligendwow@126.com>
2019-12-30 23:26:34 +0800

    注册中心 init

--- Neuron/src/main/java/com/gnerv/cloud/neuron/NeuronApplication.java
+++ Neuron/src/main/java/com/gnerv/cloud/neuron/NeuronApplication.java
@@ -1,13 +1,13 @@
-//package com.gnerv.cloud.neuron;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class NeuronApplication {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(NeuronApplication.class, args);
-//    }
-//
-//}
+package com.gnerv.cloud.neuron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeuronApplication.class, args);
+    }
+
+}
